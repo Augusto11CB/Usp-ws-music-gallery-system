3ccc2b301af9f4dd57075f264f48ed47aad7ca9b
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-11 20:43:41 -0300

    Update Readme, POM and Test class

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Usp-ws-music-gallery-system
+
+## Jena Java API
+
+
+* Model: It is a Graph
+
+### 
+
+* Model Declaration
+>```java
+> Model model = ModelFactory.createDefaultModel();
+>```
+
+* Resource Declaration
+>```java
+> String STORE_URI = "http://musicgallery/stores/rock-roll-planet"
+> Model model = ModelFactory.createDefaultModel();
+> Resource store = model.createResource(STORE_URI); 
+>```
+
+* Property Declaration
+>```java
+> String NAME_URI = "http://musicgallery/name/"
+> Model model = ModelFactory.createDefaultModel();
+> Property nameProperty = model.createResource(NAME_URI + "Rock-And-Roll-Planet"); 
+>```
+
+* Linking Property with Resource
+>```java
+> store.addProperty(nameProperty, "Rock and Roll Planet");
+>```
--- src/test/java/ws/music/gallery/system/MusicGalleryApplicationTests.java
+++ src/test/java/ws/music/gallery/system/MusicGalleryApplicationTests.java
@@ -1,13 +1,45 @@
 package ws.music.gallery.system;
 
+import org.apache.jena.rdf.model.Model;
+
+
+import org.apache.jena.rdf.model.ModelFactory;
+
+
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MusicGalleryApplicationTests {
 
+    private Model model;
+    private Resource resource;
+    private Property property;
+
+
+    @BeforeEach
+    public void init() {
+        model = ModelFactory.createDefaultModel();
+    }
+
     @Test
     void contextLoads() {
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"store1","store2"})
+    void createResourceStore(String storeURI) {
+
+        resource = model.createResource(storeURI);
+
+        System.out.println(resource);
+
     }
 
 }
