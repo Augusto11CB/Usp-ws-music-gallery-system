ba0132e6953e20e4f175a0d56f7c82b6e6e901cf
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-15 19:29:20 -0300

    Adding structure of Recommendation Controller and update in PurchaseController annotations

--- src/main/java/ws/music/gallery/system/controller/PurchaseController.java
+++ src/main/java/ws/music/gallery/system/controller/PurchaseController.java
@@ -5,5 +5,11 @@ package ws.music.gallery.system.controller;
 
 2.3 Para efetivar a compra, o usuário devera estar registrado no sistema
 */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/purchase")
 public class PurchaseController {
 }
---
+++ src/main/java/ws/music/gallery/system/controller/RecommendationController.java
@@ -0,0 +1,9 @@
+package ws.music.gallery.system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/recommendation")
+public class RecommendationController {
+}
