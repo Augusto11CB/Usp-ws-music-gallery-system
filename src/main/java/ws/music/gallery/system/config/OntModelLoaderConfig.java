2e9ca0b68c3eaef5649ac770983bf97d8bb716ce
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-17 10:56:06 -0300

    Adding Loader of owl file

---
+++ src/main/java/ws/music/gallery/system/config/OntModelLoaderConfig.java
@@ -0,0 +1,46 @@
+package ws.music.gallery.system.config;
+
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.ResourceUtils;
+
+import java.io.*;
+
+@Configuration
+//@PropertySource("classpath:application.properties")
+public class OntModelLoaderConfig {
+
+    @Value("${music.gallery.ontology.file}")
+    private String ONTOLOGY_FILE_PATH;
+
+    @Value("${music.gallery.ontology.name}")
+    private String ONTOLOGY_NAME;
+
+    @Value("${music.gallery.uri}")
+    private String MODEL_URI;
+
+
+
+
+    @Bean
+    public OntModel loadOntologyModel() {
+        OntModel ontoModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
+        try {
+
+            Resource resource = new ClassPathResource(ONTOLOGY_FILE_PATH);
+            ontoModel.read(resource.getInputStream(), null);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ontoModel;
+    }
+}
