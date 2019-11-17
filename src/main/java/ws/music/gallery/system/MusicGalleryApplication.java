1cb6d195e338e96741d143e770980bf0fb0003cb
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-17 09:28:48 -0300

    Created mock bean to read owl file

--- src/main/java/ws/music/gallery/system/MusicGalleryApplication.java
+++ src/main/java/ws/music/gallery/system/MusicGalleryApplication.java
@@ -1,7 +1,16 @@
 package ws.music.gallery.system;
 
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class MusicGalleryApplication {
@@ -10,4 +19,18 @@ public class MusicGalleryApplication {
         SpringApplication.run(MusicGalleryApplication.class, args);
     }
 
+/*    @Bean
+    public OntModel loadOntologyModel() {
+        OntModel ontoModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
+        try {
+
+            File file = new File("src/main/resources/OntologyRepository/MusicGallery.owl");
+            InputStream in = new FileInputStream(file);
+            ontoModel.read(in, null);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return ontoModel;
+    }*/
 }
