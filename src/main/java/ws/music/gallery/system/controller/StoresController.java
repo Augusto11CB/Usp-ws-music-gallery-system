492d06a7545a99b4175ea4cae61b91ad292842e7
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-17 16:36:57 -0300

    Adding calling for the storeservice from storecontroller

--- src/main/java/ws/music/gallery/system/controller/StoresController.java
+++ src/main/java/ws/music/gallery/system/controller/StoresController.java
@@ -1,10 +1,13 @@
 package ws.music.gallery.system.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ws.music.gallery.system.domain.dto.StoreDTO;
 import ws.music.gallery.system.enums.TypeProductAndBusiness;
+import ws.music.gallery.system.service.StoreService;
 
 import java.util.Collections;
 import java.util.List;
@@ -25,18 +28,31 @@ Listar as Lojas por Ordem Alfabética
 @RequestMapping(value = "/store")
 public class StoresController {
 
+    @Autowired
+    StoreService storeService;
+
     @GetMapping("/get-all")
     public List<StoreDTO> getStores() {
-        return Collections.emptyList();
+
+        return storeService.getAllStores();
     }
 
     @GetMapping("/get-all/order-by-name")
     public List<StoreDTO> getStoresOrderByName() {
-        return Collections.emptyList();
+
+        return storeService.getAllStoresOrderByName();
     }
 
-    @GetMapping("/get-all/order-by-business-type")
-    public Map<TypeProductAndBusiness, List<StoreDTO>> getStoresByBusinessType() {
+    @GetMapping("/get-stores-of-type/")
+    public List<StoreDTO> getStoresByBusinessType(
+            @RequestParam(name = "type", required = true) TypeProductAndBusiness type) {
+
+        return storeService.getStoresByBusinessType(type);
+    }
+
+    @GetMapping("/get-all/by-business-type")
+    public Map<TypeProductAndBusiness, List<StoreDTO>> getAllStoresByBusinessType() {
+
         return Collections.emptyMap();
     }
 }
