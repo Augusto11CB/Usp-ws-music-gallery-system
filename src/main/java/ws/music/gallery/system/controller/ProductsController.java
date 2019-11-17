629cffa23775330ee9d88d3e3e247d6b422dc295
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-17 18:45:12 -0300

    Added call to product service layer from controller

--- src/main/java/ws/music/gallery/system/controller/ProductsController.java
+++ src/main/java/ws/music/gallery/system/controller/ProductsController.java
@@ -1,30 +1,35 @@
 package ws.music.gallery.system.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ws.music.gallery.system.domain.dto.ProductDTO;
+import ws.music.gallery.system.enums.TypeProductAndBusiness;
+import ws.music.gallery.system.service.ProductService;
 
-import java.util.Collections;
 import java.util.List;
 
 @Controller
 @RequestMapping(value = "/product")
 public class ProductsController {
 
+    @Autowired
+    ProductService productService;
+
     @GetMapping("/get-all")
     public List<ProductDTO> getAllProducts() {
-        return Collections.emptyList();
+        return productService.getAllProducts();
     }
 
     @GetMapping("/get-products/{store-name}/")
     public List<ProductDTO> getProducstOfStore(@PathVariable(value = "store-name") String storeName) {
-        return Collections.emptyList();
+        return productService.getAllProductsOfStore(storeName);
     }
 
     @GetMapping("/get-products/{product-type}")
-    public List<ProductDTO> getProducstOfType(@PathVariable(value = "product-type") String productType) {
-        return Collections.emptyList();
+    public List<ProductDTO> getProducstOfType(@PathVariable(value = "product-type") TypeProductAndBusiness productType) {
+        return productService.getProductsByType(productType);
     }
 }
