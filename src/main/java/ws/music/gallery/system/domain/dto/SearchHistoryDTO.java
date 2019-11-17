2eb0dcfe248bc73ac4919bd589b62d379cbaca7f
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-17 09:30:40 -0300

    Adding SearchHistoryDTO

---
+++ src/main/java/ws/music/gallery/system/domain/dto/SearchHistoryDTO.java
@@ -0,0 +1,23 @@
+package ws.music.gallery.system.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchHistoryDTO {
+
+    private Long id;
+
+    private UserDTO userDTO;
+
+    private String searchMadeInSearchBar;
+
+    private List<ProductDTO> productsClicked;
+}
