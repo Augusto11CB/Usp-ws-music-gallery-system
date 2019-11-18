85c42dc16d96333485075551ea7ca294310a13f5
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-18 18:53:18 -0300

    Uploading commented swagger implementation. it needs adjustments

--- src/main/java/ws/music/gallery/system/controller/ProductsController.java
+++ src/main/java/ws/music/gallery/system/controller/ProductsController.java
@@ -5,10 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ws.music.gallery.system.domain.dto.BandTShirtDTO;
 import ws.music.gallery.system.domain.dto.ProductDTO;
+import ws.music.gallery.system.domain.dto.RecordPlayerDTO;
+import ws.music.gallery.system.domain.dto.StoreDTO;
+import ws.music.gallery.system.enums.ClothSize;
+import ws.music.gallery.system.enums.Gender;
 import ws.music.gallery.system.enums.TypeProductAndBusiness;
 import ws.music.gallery.system.service.ProductService;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -18,18 +25,63 @@ public class ProductsController {
     @Autowired
     ProductService productService;
 
+    //@ApiOperation(value = "Get all products available in the Music Gallery", response = ProductDTO.class, responseContainer = "List")
     @GetMapping("/get-all")
     public List<ProductDTO> getAllProducts() {
         return productService.getAllProducts();
     }
 
+    //@ApiOperation(value = "Get all products available in a store", response = ProductDTO.class, responseContainer = "List")
     @GetMapping("/get-products/{store-name}/")
     public List<ProductDTO> getProducstOfStore(@PathVariable(value = "store-name") String storeName) {
         return productService.getAllProductsOfStore(storeName);
     }
 
+    //@ApiOperation(value = "Get product of a certain type", response = ProductDTO.class, responseContainer = "List", notes = "See Types Available")
     @GetMapping("/get-products/{product-type}")
     public List<ProductDTO> getProducstOfType(@PathVariable(value = "product-type") TypeProductAndBusiness productType) {
         return productService.getProductsByType(productType);
+    }
+
+    @GetMapping("/test-get-products")
+    public List<ProductDTO> testGetProducts() {
+        StoreDTO pierreInstrumentosMusicais = StoreDTO.builder()
+                .name("All Rock TShirts")
+                .typeBusiness(TypeProductAndBusiness.CLOTHING)
+                .build();
+
+        StoreDTO allRockTshirts = StoreDTO.builder()
+                .name("Pierre Instrumentos Musicais")
+                .typeBusiness(TypeProductAndBusiness.CLOTHING)
+                .build();
+
+        BandTShirtDTO bandTShirtDTO = BandTShirtDTO.builder()
+                .band("Scorpions")
+                .musicalGenre("rock")
+                .size(ClothSize.P)
+                .designatedGender(Gender.MALE)
+                .mainColor("white")
+                .typeOfFiber("Jeans")
+                .name("Tshirt scorpinos nice")
+                .typeProductAndBusiness(TypeProductAndBusiness.CLOTHING)
+                .branch("scorpions")
+                .price(120)
+                .soldByStore(pierreInstrumentosMusicais)
+                .build();
+
+        RecordPlayerDTO recordPlayerDTO = RecordPlayerDTO.builder()
+                .hasRadio(true)
+                .hasRadio(false)
+                .voltage(120)
+                .typeProductAndBusiness(TypeProductAndBusiness.MUSICAL_EQUIPMENT)
+                .branch("Phillips")
+                .price(601)
+                .soldByStore(allRockTshirts)
+                .build();
+
+        ArrayList<ProductDTO> products = new ArrayList<ProductDTO>(
+                Arrays.asList(bandTShirtDTO,recordPlayerDTO));
+
+        return products;
     }
 }
--- src/main/java/ws/music/gallery/system/controller/StoresController.java
+++ src/main/java/ws/music/gallery/system/controller/StoresController.java
@@ -31,18 +31,21 @@ public class StoresController {
     @Autowired
     StoreService storeService;
 
+    //@ApiOperation(value = "Get all stores in the Music Gallery", response = StoreDTO.class, responseContainer = "List")
     @GetMapping("/get-all")
     public List<StoreDTO> getStores() {
 
         return storeService.getAllStores();
     }
 
+    //@ApiOperation(value = "Return all stores in the Music Gallery in alphabetic order", response = StoreDTO.class, responseContainer = "List")
     @GetMapping("/get-all/order-by-name")
     public List<StoreDTO> getStoresOrderByName() {
 
         return storeService.getAllStoresOrderByName();
     }
 
+    //@ApiOperation(value = "Get stores in the Music Gallery of a certain type", response = StoreDTO.class, responseContainer = "List")
     @GetMapping("/get-stores-of-type/")
     public List<StoreDTO> getStoresByBusinessType(
             @RequestParam(name = "type", required = true) TypeProductAndBusiness type) {
