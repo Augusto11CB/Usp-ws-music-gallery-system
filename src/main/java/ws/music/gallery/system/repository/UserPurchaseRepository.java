8828fa366896641a06549a76e6dbcc539c3ec160
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-24 14:56:33 -0300

    Adding new methods in UserPurchaseRepository

--- src/main/java/ws/music/gallery/system/repository/UserPurchaseRepository.java
+++ src/main/java/ws/music/gallery/system/repository/UserPurchaseRepository.java
@@ -1,6 +1,9 @@
 package ws.music.gallery.system.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ws.music.gallery.system.domain.User;
 import ws.music.gallery.system.domain.UserPurchase;
@@ -13,8 +16,12 @@ public interface UserPurchaseRepository extends CrudRepository<UserPurchase, Lon
 
     List<UserPurchase> findByProductNameLike(String name);
 
-    List<UserPurchase> findByProductType(TypeProductAndBusiness typeProduct);
+    List<UserPurchase> findByProductType(TypeProductAndBusiness typeProduct, Pageable pageable);
 
     List<UserPurchase> findByUser(User user);
+
+    @Query(value = "SELECT up FROM UserPurchase up WHERE up.user = ?1 ORDER BY up.purchaseDate")
+    List<UserPurchase> findTypesForRecommendation(@Param("user") User user);
+
 
 }
