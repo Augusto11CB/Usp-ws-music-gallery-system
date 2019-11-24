f27e8168570e95edaf388a786409d18b9c4e2364
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-24 14:57:05 -0300

    Initial implementation Recommendation Service Layer

--- src/main/java/ws/music/gallery/system/service/RecommendationService.java
+++ src/main/java/ws/music/gallery/system/service/RecommendationService.java
@@ -1,4 +1,10 @@
 package ws.music.gallery.system.service;
 
+import ws.music.gallery.system.domain.dto.ProductDTO;
+
+import java.util.List;
+
 public interface RecommendationService {
+
+    List<ProductDTO> recommendProducsSameTypeLastPurchases(String userCPF);
 }
--- src/main/java/ws/music/gallery/system/service/impl/RecommendationServiceImpl.java
+++ src/main/java/ws/music/gallery/system/service/impl/RecommendationServiceImpl.java
@@ -1,12 +1,43 @@
 package ws.music.gallery.system.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import ws.music.gallery.system.domain.User;
+import ws.music.gallery.system.domain.UserPurchase;
+import ws.music.gallery.system.domain.dto.ProductDTO;
+import ws.music.gallery.system.exception.UserNotFoundException;
+import ws.music.gallery.system.repository.UserPurchaseRepository;
+import ws.music.gallery.system.repository.UserRepository;
+import ws.music.gallery.system.service.RecommendationService;
+
+import java.util.List;
 /*
-* Send stores that user already bought some thing
-* Send products of the same type of user's last purchase
-* Recommend products Already visited*
-* */
+ * Send stores that user already bought some thing
+ * Send products of the same type of user's last purchase
+ * Recommend products Already visited*
+ * */
 
 @Service
-public class RecommendationServiceImpl {
+public class RecommendationServiceImpl implements RecommendationService {
+
+    @Autowired
+    UserPurchaseRepository userPurchaseRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public List<ProductDTO> recommendProducsSameTypeLastPurchases(String userCPF) {
+
+        User user = userRepository.findByCpfUser(userCPF)
+                .orElseThrow(() -> new UserNotFoundException("User did not found. Purchase was not Performed"));
+
+        //Pageable pageable = PageRequest.of(0, 3);
+
+        List<UserPurchase> userPurchases = userPurchaseRepository.findTypesForRecommendation(user);
+
+        return null;
+    }
 }
