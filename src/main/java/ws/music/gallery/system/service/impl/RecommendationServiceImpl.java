06f895e2e539dcfb9a5021bd742be5924467b538
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-25 17:26:40 -0300

    Implementation of RecommendationService v1

--- src/main/java/ws/music/gallery/system/service/impl/RecommendationServiceImpl.java
+++ src/main/java/ws/music/gallery/system/service/impl/RecommendationServiceImpl.java
@@ -7,12 +7,16 @@ import org.springframework.stereotype.Service;
 import ws.music.gallery.system.domain.User;
 import ws.music.gallery.system.domain.UserPurchase;
 import ws.music.gallery.system.domain.dto.ProductDTO;
+import ws.music.gallery.system.enums.TypeProductAndBusiness;
 import ws.music.gallery.system.exception.UserNotFoundException;
 import ws.music.gallery.system.repository.UserPurchaseRepository;
 import ws.music.gallery.system.repository.UserRepository;
+import ws.music.gallery.system.service.ProductService;
 import ws.music.gallery.system.service.RecommendationService;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 /*
  * Send stores that user already bought some thing
  * Send products of the same type of user's last purchase
@@ -28,16 +32,24 @@ public class RecommendationServiceImpl implements RecommendationService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    ProductService productService;
+
     @Override
     public List<ProductDTO> recommendProducsSameTypeLastPurchases(String userCPF) {
 
+        List<ProductDTO> recommendations = Collections.emptyList();
+
         User user = userRepository.findByCpfUser(userCPF)
-                .orElseThrow(() -> new UserNotFoundException("User did not found. Purchase was not Performed"));
+                .orElseThrow(() -> new UserNotFoundException("User was not found."));
 
-        //Pageable pageable = PageRequest.of(0, 3);
+        Pageable pageable = PageRequest.of(0, 3);
+        List<UserPurchase> userPurchases = userPurchaseRepository.findTypesForRecommendation(user, pageable);
 
-        List<UserPurchase> userPurchases = userPurchaseRepository.findTypesForRecommendation(user);
+        List<TypeProductAndBusiness> typesToLookingFor = userPurchases.stream().map(purchase -> purchase.getProductType()).collect(Collectors.toList());
+        typesToLookingFor.stream().forEach(type -> recommendations.addAll(productService.getProductsByType(type)));
 
-        return null;
+        return recommendations;
     }
+
 }
