09bd23680d93257247769cdd2723a391abc10552
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-12-01 11:49:00 -0300

    Adicionando wrapper de properties e controller para test

---
+++ src/main/java/ws/music/gallery/system/controller/TestController.java
@@ -0,0 +1,35 @@
+
+package ws.music.gallery.system.controller;
+
+
+import io.swagger.annotations.ApiOperation;
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ws.music.gallery.system.converter.TShirtOntologyEntitiesConverter;
+import ws.music.gallery.system.domain.dto.ProductDTO;
+
+@RestController
+public class TestController {
+
+
+    @Value("${music.gallery.uri}")
+    private String MUSIC_GALLERY_URI;
+
+    @Autowired
+    private OntModel musicGalleryOntologyModel;
+
+
+    @ApiOperation(value = "Tshirt Convert Controller Test", response = ProductDTO.class, responseContainer = "List")
+    @GetMapping("/test")
+    public ProductDTO testTshirtConverterIndividualToProductDTO() {
+        //Individual indv = musicGalleryOntologyModel.getIndividual(MUSIC_GALLERY_URI + "ComfortableTShirt");
+        //return tShirtOntologyEntitiesConverter.individualToProductDTO(indv);
+        return null;
+    }
+}
+
---
+++ src/main/java/ws/music/gallery/system/util/OntologyPropertyAndResourceUtils.java
@@ -0,0 +1,42 @@
+package ws.music.gallery.system.util;
+
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.impl.PropertyImpl;
+
+
+public class OntologyPropertyAndResourceUtils {
+
+
+    public static String MUSIC_GALLERY_URI = "http://www.musicgallery/";
+
+    public static Property name = new PropertyImpl(MUSIC_GALLERY_URI + "name");
+    public static Property price = new PropertyImpl(MUSIC_GALLERY_URI + "price");
+    public static Property brand = new PropertyImpl(MUSIC_GALLERY_URI + "brand");
+    public static Property typeIs = new PropertyImpl(MUSIC_GALLERY_URI + "typeIs");
+    public static Property soldByStore = new PropertyImpl(MUSIC_GALLERY_URI + "soldByStore");
+
+    public static Property mainColor = new PropertyImpl(MUSIC_GALLERY_URI + "mainColor");
+    public static Property size = new PropertyImpl(MUSIC_GALLERY_URI + "clothSize");
+    public static Property typeOfFiber = new PropertyImpl(MUSIC_GALLERY_URI + "typeOfFiber");
+    public static Property designatedGender = new PropertyImpl(MUSIC_GALLERY_URI + "gender");
+
+    public static Property band = new PropertyImpl(MUSIC_GALLERY_URI + "band");
+
+    public static Property musicalGenre = new PropertyImpl(MUSIC_GALLERY_URI + "musicalGenre");
+    public static Property releaseYear = new PropertyImpl(MUSIC_GALLERY_URI + "releaseYear");
+
+    public static Property typeBusiness = new PropertyImpl(MUSIC_GALLERY_URI + "typeBusiness");
+
+    public static Property hasRadio = new PropertyImpl(MUSIC_GALLERY_URI + "hasRadio");
+    public static Property hasUSBPort = new PropertyImpl(MUSIC_GALLERY_URI + "hasUSBPort");
+    public static Property voltage = new PropertyImpl(MUSIC_GALLERY_URI + "voltage");
+
+    public static Property typeOfInstrument = new PropertyImpl(MUSIC_GALLERY_URI + "typeOfInstrument");
+    public static Property isSecondHand = new PropertyImpl(MUSIC_GALLERY_URI + "isSecondHand");
+
+    public static String tshirtClass =  MUSIC_GALLERY_URI +  "TShirt";
+    public static String recordPlayer = MUSIC_GALLERY_URI +  "RecordPlayer";
+    public static String musicalInstrument = MUSIC_GALLERY_URI +  "MusicalInstrument";
+    public static String vynl = MUSIC_GALLERY_URI +  "Vynl";
+
+}
