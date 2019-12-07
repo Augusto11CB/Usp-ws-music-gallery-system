dcdfee493c872f191395a3521c0fa94df0ee8ccf
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-12-07 19:22:24 -0300

    change db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,34 +6,34 @@
 CREATE SCHEMA  IF NOT EXISTS  MUSIC_GALLERY;
 
 
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(1,100,'http://www.musicgallery/VitrolaSuperboxUitech');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(2,100,'http://www.musicgallery/ARevoltaDosDandis');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(3,100,'http://www.musicgallery/BeeGeesTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(4,100,'http://www.musicgallery/BlackTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(5,100,'http://www.musicgallery/CloseToTheEdge');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(6,100,'http://www.musicgallery/ComfortableTShirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(7,100,'http://www.musicgallery/ElectricGuitar');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(8,100,'http://www.musicgallery/GIdleTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(9,100,'http://www.musicgallery/JoJoTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(10,100,'http://www.musicgallery/KanaBoonTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(11,100,'http://www.musicgallery/KimetsuTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(12,100,'http://www.musicgallery/KissLoveGun1997');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(13,100,'http://www.musicgallery/McIguTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(14,100,'http://www.musicgallery/OnePieceTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(15,100,'http://www.musicgallery/RedHotTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(16,100,'http://www.musicgallery/Spark');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(17,100,'http://www.musicgallery/Theremin');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(18,100,'http://www.musicgallery/UprightPiano');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(19,100,'http://www.musicgallery/Violin');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(20,100,'http://www.musicgallery/VitrolaPhillips');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(21,100,'http://www.musicgallery/VitrolaRaveoOpera');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(22,100,'http://www.musicgallery/VitrolaRaveoSonetto');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(23,100,'http://www.musicgallery/VitrolaSuperboxUitech');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(24,100,'http://www.musicgallery/VitrolaWoodburnTabaco');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(25,100,'http://www.musicgallery/WhiteTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(26,100,'http://www.musicgallery/YesTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(27,100,'http://www.musicgallery/ZikaTshirt');
-insert into MUSIC_GALLERY.product (id_user_purchase, available_quantity, product_url) values(28,100,'http://www.musicgallery/BeeGeesTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(2,100,'http://www.musicgallery/ARevoltaDosDandis');
+insert into product (id_user_purchase, available_quantity, product_url) values(3,100,'http://www.musicgallery/BeeGeesTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(4,100,'http://www.musicgallery/BlackTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(5,100,'http://www.musicgallery/CloseToTheEdge');
+insert into product (id_user_purchase, available_quantity, product_url) values(6,100,'http://www.musicgallery/ComfortableTShirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(7,100,'http://www.musicgallery/ElectricGuitar');
+insert into product (id_user_purchase, available_quantity, product_url) values(8,100,'http://www.musicgallery/GIdleTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(9,100,'http://www.musicgallery/JoJoTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(10,100,'http://www.musicgallery/KanaBoonTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(11,100,'http://www.musicgallery/KimetsuTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(12,100,'http://www.musicgallery/KissLoveGun1997');
+insert into product (id_user_purchase, available_quantity, product_url) values(13,100,'http://www.musicgallery/McIguTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(15,100,'http://www.musicgallery/RedHotTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(14,100,'http://www.musicgallery/OnePieceTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(16,100,'http://www.musicgallery/Spark');
+insert into product (id_user_purchase, available_quantity, product_url) values(1,100,'http://www.musicgallery/VitrolaSuperboxUitech');
+insert into product (id_user_purchase, available_quantity, product_url) values(17,100,'http://www.musicgallery/Theremin');
+insert into product (id_user_purchase, available_quantity, product_url) values(18,100,'http://www.musicgallery/UprightPiano');
+insert into product (id_user_purchase, available_quantity, product_url) values(19,100,'http://www.musicgallery/Violin');
+insert into product (id_user_purchase, available_quantity, product_url) values(20,100,'http://www.musicgallery/VitrolaPhillips');
+insert into product (id_user_purchase, available_quantity, product_url) values(21,100,'http://www.musicgallery/VitrolaRaveoOpera');
+insert into product (id_user_purchase, available_quantity, product_url) values(22,100,'http://www.musicgallery/VitrolaRaveoSonetto');
+insert into product (id_user_purchase, available_quantity, product_url) values(23,100,'http://www.musicgallery/VitrolaSuperboxUitech');
+insert into product (id_user_purchase, available_quantity, product_url) values(24,100,'http://www.musicgallery/VitrolaWoodburnTabaco');
+insert into product (id_user_purchase, available_quantity, product_url) values(25,100,'http://www.musicgallery/WhiteTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(26,100,'http://www.musicgallery/YesTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(27,100,'http://www.musicgallery/ZikaTshirt');
+insert into product (id_user_purchase, available_quantity, product_url) values(28,100,'http://www.musicgallery/BeeGeesTshirt');
 
 
 -- insert into MUSIC_GALLERY.user(id_user,age,cpf_user,gender,password)  values (1,22,'42734758694','MALE','123')
